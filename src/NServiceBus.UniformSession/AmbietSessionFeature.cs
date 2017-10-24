7d3565bedfda7c9af9a3c104fad0cbe3c9b2388d
danielmarbach <daniel.marbach@openplace.net>
2017-10-24 10:49:58 +0200

    Initial structure

---
+++ src/NServiceBus.UniformSession/AmbietSessionFeature.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus.Features;
+
+namespace NServiceBus.AmbientSession
+{
+    public class AmbietSessionFeature : Feature
+    {
+        CurrentSessionHolder sessionHolder = new CurrentSessionHolder();
+
+        public AmbietSessionFeature()
+        {
+            EnableByDefault();
+        }
+
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            context.Pipeline.Register(new RegisterCurrentSessionBehavior(sessionHolder), "register the current session in the ambiet bus session.");
+            context.RegisterStartupTask(new RegisterSessionStartupTask(sessionHolder));
+
+            context.Container.ConfigureComponent<IBusSession>(() => sessionHolder.Current, DependencyLifecycle.InstancePerCall);
+        }
+
+        private class RegisterSessionStartupTask : FeatureStartupTask
+        {
+            private readonly CurrentSessionHolder sessionHolder;
+            IDisposable scope;
+
+            public RegisterSessionStartupTask(CurrentSessionHolder sessionHolder)
+            {
+                this.sessionHolder = sessionHolder;
+            }
+
+            protected override Task OnStart(IMessageSession session)
+            {
+                scope = sessionHolder.SetMessageSession(session);
+                return Task.CompletedTask;
+            }
+
+            protected override Task OnStop(IMessageSession session)
+            {
+                scope.Dispose();
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
---
+++ src/NServiceBus.UniformSession/RegisterCurrentSessionBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus.Pipeline;
+
+namespace NServiceBus.AmbientSession
+{
+    class RegisterCurrentSessionBehavior : IBehavior<IIncomingPhysicalMessageContext, IIncomingPhysicalMessageContext>
+    {
+        private readonly CurrentSessionHolder sessionHolder;
+
+        public RegisterCurrentSessionBehavior(CurrentSessionHolder sessionHolder)
+        {
+            this.sessionHolder = sessionHolder;
+        }
+
+        public async Task Invoke(IIncomingPhysicalMessageContext context, Func<IIncomingPhysicalMessageContext, Task> next)
+        {
+            using (sessionHolder.SetCurrentPipelineContext(context))
+            {
+                await next(context).ConfigureAwait(false);
+            }
+        }
+    }
+}
