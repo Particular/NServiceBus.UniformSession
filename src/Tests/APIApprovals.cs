dde758914af325a3b7b1e968b636a05cf060e76c
danielmarbach <daniel.marbach@openplace.net>
2017-10-24 11:09:50 +0200

    Tests including APIApprovals

---
+++ src/Tests/APIApprovals.cs
@@ -0,0 +1,32 @@
+﻿namespace NServiceBus.UniformSession.Tests
+{
+    using System;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using NUnit.Framework;
+    using PublicApiGenerator;
+    using UniformSession;
+
+    [TestFixture]
+    public class APIApprovals
+    {
+        [Test]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void ApproveNServiceBus()
+        {
+            var publicApi = Filter(ApiGenerator.GeneratePublicApi(typeof(IUniformSession).Assembly));
+            TestApprover.Verify(publicApi);
+        }
+
+        string Filter(string text)
+        {
+            return string.Join(Environment.NewLine, text.Split(new[]
+            {
+                Environment.NewLine
+            }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(l => !l.StartsWith("[assembly: ReleaseDateAttribute("))
+                .Where(l => !string.IsNullOrWhiteSpace(l))
+                );
+        }
+    }
+}
