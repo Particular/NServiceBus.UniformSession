0b7c36f37f957d149dc780eeb092366c7bf22ecc
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-12-01 15:29:32 +0100

    support mixed test context usage

--- src/NServiceBus.UniformSession.Testing.Tests/TestableUniformSessionTests.cs
+++ src/NServiceBus.UniformSession.Testing.Tests/TestableUniformSessionTests.cs
@@ -5,17 +5,67 @@
     using System.Threading.Tasks;
     using NServiceBus.Testing;
     using NUnit.Framework;
+    using Pipeline;
 
     [TestFixture]
     public class TestableUniformSessionTests
     {
+        [Test]
+        public async Task Shares_state_with_handler_context()
+        {
+            var handlerContext = new TestableMessageHandlerContext();
+            var uniformSession = new TestableUniformSession(handlerContext);
+            var handler = new SendingHandler(uniformSession);
+
+            await handler.Handle(new SomeCommand(), handlerContext);
+
+            Assert.AreEqual(2, uniformSession.PublishedMessages.Length);
+            Assert.AreEqual(2, uniformSession.SentMessages.Length);
+            Assert.AreEqual(2, handlerContext.PublishedMessages.Length);
+            Assert.AreEqual(2, handlerContext.SentMessages.Length);
+
+            Assert.IsTrue(handlerContext.DoNotContinueDispatchingCurrentMessageToHandlersWasCalled);
+        }
+
+        [Test]
+        public async Task Shares_state_with_pipeline_context()
+        {
+            var pipelineContext = new TestableIncomingLogicalMessageContext();
+            var uniformSession = new TestableUniformSession(pipelineContext);
+            var behavior = new SendingBehavior(uniformSession);
+
+            await behavior.Invoke(pipelineContext, () => Task.CompletedTask);
+
+            Assert.AreEqual(2, uniformSession.PublishedMessages.Length);
+            Assert.AreEqual(2, uniformSession.SentMessages.Length);
+            Assert.AreEqual(2, pipelineContext.PublishedMessages.Length);
+            Assert.AreEqual(2, pipelineContext.SentMessages.Length);
+
+            Assert.AreEqual("testValue", pipelineContext.Headers["testHeader"]);
+        }
+
+        [Test]
+        public async Task Shares_state_with_message_session()
+        {
+            var messageSession = new TestableMessageSession();
+            var uniformSession = new TestableUniformSession(messageSession);
+            var component = new MessageSessionComponent(uniformSession);
+
+            await component.DoSomething(messageSession);
+
+            Assert.AreEqual(2, uniformSession.PublishedMessages.Length);
+            Assert.AreEqual(2, uniformSession.SentMessages.Length);
+            Assert.AreEqual(2, messageSession.PublishedMessages.Length);
+            Assert.AreEqual(2, messageSession.SentMessages.Length);
+        }
+
         [Test]
         public async Task When_used_to_test_another_component_collects_outgoing_messages()
         {
             var uniformSession = new TestableUniformSession();
             var reusableComponent = new ReusableComponent(uniformSession);
 
-            await reusableComponent.FireCommand().ConfigureAwait(false);
+            await reusableComponent.FireCommand();
 
             Assert.AreEqual(1, uniformSession.SentMessages.Length);
         }
@@ -26,7 +76,7 @@
             var uniformSession = new TestableUniformSession();
             var reusableComponent = new ReusableComponent(uniformSession);
 
-            await reusableComponent.PublishEvent().ConfigureAwait(false);
+            await reusableComponent.PublishEvent();
 
             Assert.AreEqual(1, uniformSession.PublishedMessages.Length);
         }
@@ -45,6 +95,79 @@
             public Task PublishEvent(CancellationToken cancellationToken = default) => session.Publish(new SomeEvent(), cancellationToken: cancellationToken);
         }
 
+        class SendingHandler : IHandleMessages<SomeCommand>
+        {
+            IUniformSession uniformSession;
+
+            public SendingHandler(IUniformSession uniformSession)
+            {
+                this.uniformSession = uniformSession;
+            }
+
+            public async Task Handle(SomeCommand message, IMessageHandlerContext context)
+            {
+                await context.Send(new SomeCommand());
+                await context.Publish(new SomeEvent());
+                await uniformSession.Send(new SomeCommand());
+                await uniformSession.Publish(new SomeEvent());
+
+                context.DoNotContinueDispatchingCurrentMessageToHandlers();
+            }
+        }
+
+        class SendingBehavior : Behavior<IIncomingLogicalMessageContext>
+        {
+            IUniformSession uniformSession;
+
+            public SendingBehavior(IUniformSession uniformSession)
+            {
+                this.uniformSession = uniformSession;
+            }
+
+            public override async Task Invoke(IIncomingLogicalMessageContext context, Func<Task> next)
+            {
+                await uniformSession.Send(new SomeCommand());
+                await uniformSession.Publish(new SomeEvent());
+                await context.Send(new SomeCommand());
+                await context.Publish(new SomeEvent());
+                await next();
+
+                context.Headers["testHeader"] = "testValue";
+            }
+        }
+
+        class MessageSessionComponent
+        {
+            IUniformSession uniformSession;
+
+            public MessageSessionComponent(IUniformSession uniformSession)
+            {
+                this.uniformSession = uniformSession;
+            }
+
+#pragma warning disable PS0018 // A task-returning method should have a CancellationToken parameter unless it has a parameter implementing ICancellableContext
+            public async Task DoSomething(IMessageSession messageSession)
+#pragma warning restore PS0018 // A task-returning method should have a CancellationToken parameter unless it has a parameter implementing ICancellableContext
+            {
+                await messageSession.Send<ISomeOtherCommand>(_ => { });
+                await messageSession.Publish<ISomeOtherEvent>(_ => { });
+                await uniformSession.Send<ISomeOtherCommand>(_ => { });
+                await uniformSession.Publish<ISomeOtherEvent>(_ => { });
+            }
+        }
+
+        // Disable warning because public type is necessary for interface messages
+        // ReSharper disable once MemberCanBePrivate.Global
+        public interface ISomeOtherCommand : ICommand
+        {
+        }
+
+        // Disable warning because public type is necessary for interface messages
+        // ReSharper disable once MemberCanBePrivate.Global
+        public interface ISomeOtherEvent : IEvent
+        {
+        }
+
         class SomeCommand : ICommand
         {
             public Guid CustomerId { get; set; }
@@ -52,36 +175,6 @@
 
         class SomeEvent : IEvent
         {
-
-        }
-
-        class SomeHandler : IHandleMessages<SomeCommand>
-        {
-            readonly IUniformSession session;
-
-            public SomeHandler(IUniformSession session) => this.session = session;
-
-            public Task Handle(SomeCommand message, IMessageHandlerContext context)
-                => session.Publish(new SomeEvent());
-        }
-
-        class SomeSagaData : ContainSagaData
-        {
-            public Guid CustomerId { get; set; }
-        }
-
-        class SomeSaga : NServiceBus.Saga<SomeSagaData>, IAmStartedByMessages<SomeCommand>
-        {
-            readonly IUniformSession session;
-
-            public SomeSaga(IUniformSession session) => this.session = session;
-
-            protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SomeSagaData> mapper)
-                => mapper.MapSaga(saga => saga.CustomerId)
-                    .ToMessage<SomeCommand>(command => command.CustomerId);
-
-            public Task Handle(SomeCommand message, IMessageHandlerContext context)
-                => session.Publish<SomeEvent>();
         }
     }
 }
