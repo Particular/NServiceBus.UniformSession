0c580d81cf643cf65981251d2776a880402f9b8d
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-12-06 14:34:23 +0100

    Uniform testing update (#223)
    * add test editorconfig settings
    
    * support mixed test context usage
    
    * approve public API changes
    
    * disable PS0018 for whole test project
    
    * remove R# warnings

--- src/NServiceBus.UniformSession.Testing/TestableUniformSession.cs
+++ src/NServiceBus.UniformSession.Testing/TestableUniformSession.cs
@@ -5,48 +5,39 @@
     using System.Threading.Tasks;
     using NServiceBus.Testing;
 
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Code", "PS0002:Instance methods on types implementing ICancellableContext should not have a CancellationToken parameter", Justification = "Testable object implementing interface explicitly")]
-    public class TestableUniformSession : TestablePipelineContext, IUniformSession
+    public class TestableUniformSession : IUniformSession
     {
-        TestableMessageHandlerContext inner;
+        TestablePipelineContext innerContext;
+        TestableMessageSession innerSession;
 
-        internal void SetInner(TestableMessageHandlerContext context)
-            => inner = context;
-
-        async Task IUniformSession.Send(object message, SendOptions options, CancellationToken cancellationToken)
+        public TestableUniformSession() : this(new TestablePipelineContext())
         {
-            if (inner != null)
-            {
-                await inner.Send(message, options).ConfigureAwait(false);
-            }
-            await base.Send(message, options).ConfigureAwait(false);
         }
 
-        async Task IUniformSession.Send<T>(Action<T> messageConstructor, SendOptions options, CancellationToken cancellationToken)
+        public TestableUniformSession(TestableMessageSession innerSession)
         {
-            if (inner != null)
-            {
-                await inner.Send(messageConstructor, options).ConfigureAwait(false);
-            }
-            await base.Send(messageConstructor, options).ConfigureAwait(false);
+            this.innerSession = innerSession;
         }
 
-        async Task IUniformSession.Publish(object message, PublishOptions options, CancellationToken cancellationToken)
+        public TestableUniformSession(TestablePipelineContext innerContext)
         {
-            if (inner != null)
-            {
-                await inner.Publish(message, options).ConfigureAwait(false);
-            }
-            await base.Publish(message, options).ConfigureAwait(false);
+            this.innerContext = innerContext;
         }
 
-        async Task IUniformSession.Publish<T>(Action<T> messageConstructor, PublishOptions publishOptions, CancellationToken cancellationToken)
-        {
-            if (inner != null)
-            {
-                await inner.Publish(messageConstructor, publishOptions).ConfigureAwait(false);
-            }
-            await base.Publish(messageConstructor, publishOptions).ConfigureAwait(false);
-        }
+        public SentMessage<object>[] SentMessages => innerContext?.SentMessages ?? innerSession.SentMessages;
+
+        public PublishedMessage<object>[] PublishedMessages => innerContext?.PublishedMessages ?? innerSession.PublishedMessages;
+
+        Task IUniformSession.Send(object message, SendOptions options, CancellationToken cancellationToken)
+            => innerContext?.Send(message, options) ?? innerSession.Send(message, options, cancellationToken);
+
+        Task IUniformSession.Send<T>(Action<T> messageConstructor, SendOptions options, CancellationToken cancellationToken)
+            => innerContext?.Send(messageConstructor, options) ?? innerSession.Send(messageConstructor, options, cancellationToken);
+
+        Task IUniformSession.Publish(object message, PublishOptions options, CancellationToken cancellationToken)
+            => innerContext?.Publish(message, options) ?? innerSession.Publish(message, options, cancellationToken);
+
+        Task IUniformSession.Publish<T>(Action<T> messageConstructor, PublishOptions publishOptions, CancellationToken cancellationToken)
+            => innerContext?.Publish(messageConstructor, publishOptions) ?? innerSession.Publish(messageConstructor, publishOptions, cancellationToken);
     }
 }
