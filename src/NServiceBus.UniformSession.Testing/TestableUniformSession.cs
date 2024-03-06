c801aaf1abdba505ab6f238d747cd1de2495f7d0
Brandon Ording <bording@gmail.com>
2024-03-06 15:21:28 -0500

    Tweaks

--- src/NServiceBus.UniformSession.Testing/TestableUniformSession.cs
+++ src/NServiceBus.UniformSession.Testing/TestableUniformSession.cs
@@ -5,27 +5,47 @@
     using System.Threading.Tasks;
     using NServiceBus.Testing;
 
+    /// <summary>
+    /// A testable implementation of <see cref="IUniformSession" />.
+    /// </summary>
     public class TestableUniformSession : IUniformSession
     {
-        TestablePipelineContext innerContext;
-        TestableMessageSession innerSession;
+        readonly TestablePipelineContext innerContext;
+        readonly TestableMessageSession innerSession;
 
+        /// <summary>
+        /// Creates a new instance of <see cref="TestableUniformSession" />.
+        /// </summary>
         public TestableUniformSession() : this(new TestablePipelineContext())
         {
         }
 
+        /// <summary>
+        /// Creates a new instance of <see cref="TestableUniformSession" />.
+        /// </summary>
+        /// <param name="innerSession">The <see cref="TestableMessageSession" /> the session should use to send and publish messages.</param>
         public TestableUniformSession(TestableMessageSession innerSession)
         {
             this.innerSession = innerSession;
         }
 
+        /// <summary>
+        /// Creates a new instance of <see cref="TestableUniformSession" />.
+        /// </summary>
+        /// <param name="innerContext">The <see cref="TestablePipelineContext" /> the session should use to send and publish messages.</param>
         public TestableUniformSession(TestablePipelineContext innerContext)
         {
             this.innerContext = innerContext;
         }
 
+        /// <summary>
+        /// The messages that have been sent from the session.
+        /// </summary>
         public SentMessage<object>[] SentMessages => innerContext?.SentMessages ?? innerSession.SentMessages;
 
+        /// <summary>
+        /// The messages that have been published from the session.
+        /// </summary>
         public PublishedMessage<object>[] PublishedMessages => innerContext?.PublishedMessages ?? innerSession.PublishedMessages;
 
         Task IUniformSession.Send(object message, SendOptions options, CancellationToken cancellationToken)
