729a168fa255be0aa781f6f0e1253ff2dff85e68
Daniel Marbach <danielmarbach@users.noreply.github.com>
2024-08-29 09:54:49 +0200

    Autoformat of test projects for NUnit2018

--- src/AcceptanceTests/When_sending_from_cached_message_session_after_shutdown.When_sending_from_cached_message_session_after_shutdown.cs
+++ src/AcceptanceTests/When_sending_from_cached_message_session_after_shutdown.When_sending_from_cached_message_session_after_shutdown.cs
@@ -21,7 +21,7 @@
                 .Done(c => c.EndpointsStarted)
                 .Run();
 
-            Assert.NotNull(ctx.StartupUniformSession);
+            Assert.That(ctx.StartupUniformSession, Is.Not.Null);
             var exception = Assert.ThrowsAsync<InvalidOperationException>(() => ctx.StartupUniformSession.SendLocal(new MyMessage()));
             StringAssert.Contains("The endpoint owning this 'IUniformSession' instance has been stopped, so it is no longer possible to execute message operations.", exception.Message);
         }
